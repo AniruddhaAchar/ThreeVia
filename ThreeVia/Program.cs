9ae0e26476330f99eaf2623424a8f23983b7af5b
Aniruddha Achar <aniruddha.achar@gmail.com>
2022-06-30 17:45:54 -0700

    Added questio building capability.

--- ThreeVia/Program.cs
+++ ThreeVia/Program.cs
@@ -1,6 +1,6 @@
 using Microsoft.AspNetCore.Components;
 using Microsoft.AspNetCore.Components.Web;
-using ThreeVia.Data;
+using ThreeVia.Model;
 using ThreeVia.Logic;
 
 var builder = WebApplication.CreateBuilder(args);
---
+++ ThreeVia/ViewModel/Tournament.cs
@@ -0,0 +1,11 @@
+﻿using ThreeVia.Model.Game;
+
+namespace ThreeVia.ViewModel
+{
+    public class Tournament
+    {
+        public int NumberOfRounds { get; set; }
+
+        public Round  Rounds { get; set; }
+    }
+}
